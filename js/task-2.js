d0922ef7da0c919966ce67c86f429af128327726
Andrii-Turyk88 <turykandrii888@gmail.com>
2026-03-15 10:07:17 +0100

    Fix index.html and update tasks 2 and 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,15 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
   let total = 0;
 
   for (const day of days) {
     total += day.calories;
   }
 
-  return total / days.lenght;
+  return total / days.length;
 }
 
 console.log(
